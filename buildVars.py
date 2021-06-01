f6d93f69a07deda50534d461a99f92d68db34869
zstanecic <zvonimirek222@yandex.com>
2021-06-01 17:39:26 +0200

    corrected the info about the addon in manifest

--- buildVars.py
+++ buildVars.py
@@ -20,7 +20,7 @@ addon_info = {
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : [
-	_("Adds local temperature and forecast 24 ours and prediction up to 2 days.\n"),
+	_("Adds local temperature and forecast 24 ours and prediction up to 3 days.\n"),
 	_("NVDA+w for the current bulletin.\n"),
 	_("NVDA+shift+w for the prediction in the current 24 hours and prediction up to 9 days.\n"),
 	_("NVDA+shift+control+w for quickly enter a temporary city.\n"),
